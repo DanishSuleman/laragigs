888f5d189ee8d32008cb391ce6a50ca40e7f25db
Danish Suleman <danish.sulaiman@outlook.com>
2023-11-18 05:39:02 +0500

    added initial project files

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+@section('content')
+@include('partials._hero')
+@include('partials._search')
+
+@if (count($listings) > 0)
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+    @foreach ($listings as $listing)
+    <x-listing-card :listing="$listing" />
+    @endforeach
+    @else
+    <p>No Listings Found</p>
+    @endif
+</div>
+@endsection
